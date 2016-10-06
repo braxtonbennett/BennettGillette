d9a131a47d1d8364afaa5ec394e77e28c05f0562
Douglas <Douglas@10.38.164.117>
2016-10-06 13:30:08 -0600

    10/6/2016 created model package and player class

--- hemorrhage/src/hemorrhage/Hemorrhage.java
+++ hemorrhage/src/hemorrhage/Hemorrhage.java
@@ -5,6 +5,8 @@
  */
 package hemorrhage;
 
+import byui.cit260.hemorrhage.model.Player;
+
 /**
  *
  * @author Douglas
@@ -15,7 +17,14 @@ public class Hemorrhage {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Player playerOne = new Player();
+        
+        playerOne.setName("Charlie");
+        playerOne.setBestTime(2.00);
+        
+        String playerInfo = playerOne.toString();
+        System.out.println(playerInfo);
     }
     
 }
