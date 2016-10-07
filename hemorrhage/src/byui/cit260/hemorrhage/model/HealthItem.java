db8b9beb8d5294cea67ec6ed3a042d28d190d62c
Douglas <Douglas@10.38.164.117>
2016-10-07 12:13:47 -0600

    10/7/2016 12:12 created classes : Character, Game, Item, and HealthItem. also changed Hemorrhage.java

---
+++ hemorrhage/src/byui/cit260/hemorrhage/model/HealthItem.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.hemorrhage.model;
+
+/**
+ *
+ * @author Douglas
+ */
+public class HealthItem extends Item {
+    
+    private long healthPoints;
+
+    public HealthItem() {
+    }
+
+    public long getHealthPoints() {
+        return healthPoints;
+    }
+
+    public void setHealthPoints(long healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+
+    @Override
+    public String toString() {
+        return "HealthItem{" + "healthPoints=" + healthPoints + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + (int) (this.healthPoints ^ (this.healthPoints >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final HealthItem other = (HealthItem) obj;
+        if (this.healthPoints != other.healthPoints) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
--- hemorrhage/src/hemorrhage/Hemorrhage.java
+++ hemorrhage/src/hemorrhage/Hemorrhage.java
@@ -5,7 +5,11 @@
  */
 package hemorrhage;
 
+import byui.cit260.hemorrhage.model.Game;
 import byui.cit260.hemorrhage.model.Player;
+import byui.cit260.hemorrhage.model.Character;
+import byui.cit260.hemorrhage.model.HealthItem;
+import byui.cit260.hemorrhage.model.Item;
 
 /**
  *
@@ -25,6 +29,40 @@ public class Hemorrhage {
         
         String playerInfo = playerOne.toString();
         System.out.println(playerInfo);
+        
+        Game gameOne = new Game();
+        gameOne.setTotalTime(2.55);
+        gameOne.setNoPeople(5);
+        
+        String gameInfo = gameOne.toString();
+        System.out.print(gameInfo);
+        
+        Character suzie = new Character();
+        suzie.setName("Suzie");
+        suzie.setDescription("skinny girl with dark blach hair");
+        suzie.setMapCoordinateX(2);
+        suzie.setMapCoordinateY(3);
+        suzie.setAtkPower(35);
+        suzie.setHealth(100);
+        suzie.setDefPower(10);
+        
+        String characterInfo = suzie.toString();
+        System.out.println(characterInfo);
+        
+        Item itemOne = new Item();
+        itemOne.setName("Poison");
+        itemOne.setQuantityInStock(1);
+        
+        String itemInfo = itemOne.toString();
+        System.out.println(itemInfo);
+        
+        HealthItem bread = new HealthItem();
+        bread.setName("Bread");
+        bread.setQuantityInStock(5);
+        bread.setHealthPoints(30);
+        
+        String healthItemInfo = bread.toString();
+        System.out.println(healthItemInfo);
     }
     
 }
