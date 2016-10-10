5c79ba4f3575d21c3fc05ee7d1ef0b517ba68ceb
Braxton <Braxton@10.35.165.114>
2016-10-10 13:25:34 -0600

    added comment to player class

---
+++ hemorrhage/src/byui/cit260/hemorrhage/model/Pack.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.hemorrhage.model;
+
+/**
+ *
+ * @author Braxton
+ */
+public class Pack {
+    public int noSlots;
+    
+    public int getNoSlots();
+        return noSlots;
+        
+    public void setNoSlots(noSlots);
+    
+}
