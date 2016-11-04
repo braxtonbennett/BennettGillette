ff03f3b6501a0bcd1d891e71447cea185339e9ec
jnava <jnava@10.38.165.189>
2016-11-04 13:36:18 -0600

    10/5/2016 Created Project

--- hemorrhage/src/hemorrhage/Hemorrhage.java
+++ hemorrhage/src/hemorrhage/Hemorrhage.java
@@ -7,6 +7,7 @@ package hemorrhage;
 
 
 import byui.cit260.hemorrhage.model.Game;
+import byui.cit260.hemorrhage.model.HealthItem;
 import byui.cit260.hemorrhage.model.Player;
 import byui.cit260hemmorrhage.view.StartProgramView;
 
@@ -19,13 +20,24 @@ public class Hemorrhage {
 
     private static Game currentGame = null;
     private static Player player = null;
-    
+    private static HealthItem chicken = new HealthItem();
+    private static HealthItem steak = new HealthItem();
+    private static HealthItem burger = new HealthItem();
        
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
        
+        chicken.setHealthPoints(10);
+            chicken.setQuantityInStock(5);
+            
+         steak.setHealthPoints(25);
+            steak.setQuantityInStock(5);
+            
+          burger.setHealthPoints(50);
+            burger.setQuantityInStock(5);
+            
        StartProgramView startProgramView = new StartProgramView();
        startProgramView.displayStartProgramView();
              
@@ -46,6 +58,30 @@ public class Hemorrhage {
 
     public static void setPlayer(Player player) {
         Hemorrhage.player = player;
+    }
+
+    public static HealthItem getChicken() {
+        return chicken;
+    }
+
+    public static void setChicken(HealthItem chicken) {
+        Hemorrhage.chicken = chicken;
+    }
+
+    public static HealthItem getSteak() {
+        return steak;
+    }
+
+    public static void setSteak(HealthItem steak) {
+        Hemorrhage.steak = steak;
+    }
+
+    public static HealthItem getBurger() {
+        return burger;
+    }
+
+    public static void setBurger(HealthItem burger) {
+        Hemorrhage.burger = burger;
     }
     
 }
